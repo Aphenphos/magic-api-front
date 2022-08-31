b42fb3e908af483697520dd971e7694635892125
Aphenphos <MaxwillWinters@gmail.com>
2022-08-31 14:59:19 -0400

    displays legalities in a readable way

--- utils/utils.js
+++ utils/utils.js
@@ -7,5 +7,33 @@ export async function checkIfNull(defaultValue, ifNotNull) {
 }
 
 export async function displayPrices(USD, USDFoil, EUR, EURFoil) {
-    
+    let USDnull = await checkIfNull('', USD);
+    let USDFoilnull = await checkIfNull('', USDFoil);
+    let EURnull = await checkIfNull('', EUR);
+    let EURFoilnull = await checkIfNull('', EURFoil);
+
+    if (USDnull !== '') {
+        USDnull = `$${USDnull}`;
+    }
+    if (USDFoilnull !== '') {
+        USDFoilnull = `$${USDFoilnull}`;
+    }
+    if (EURnull !== '') {
+        EURnull = `€${EURnull}`;
+    }
+    if (EURFoilnull !== '') {
+        EURFoilnull = `€${EURFoilnull}`;
+    }
+    const displayedValue = `${USDnull}  ${USDFoilnull}  ${EURnull}  ${EURFoilnull}`;
+    return displayedValue;
+}
+
+export async function displayLegalities(legalities) {
+    let input = legalities.toString();
+    let displayedValue = input.replaceAll('not_legal', '❌\n')
+        .replaceAll('banned,', '⦻\n')
+        .replaceAll('legal', '✅\n')
+        .replaceAll('restricted', '⚠\n')
+        .replaceAll(',', '');
+    return displayedValue;
 }
